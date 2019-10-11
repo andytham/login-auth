d1e9a18e34f9485ef4895007a3b2c94de49246f1
andy <thamandyy@gmail.com>
2019-10-11 00:24:03 -0400

    Update imports

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,27 +1,52 @@
-import React, { useState, createContext } from 'react';
-import user, { UserProvider } from './context';
-import Home from './Home';
+import React, { useState, useContext } from 'react';
+
+import { AuthContext } from '../context/auth'
+import { UserContext } from '../context/user';
 
 export default function Login(){
-	const [name, setName] = useState('test') 
-
-	function handleNameChange(e){
-		setName(e.target.value)
+	const user = useContext(UserContext)
+	const auth = useContext(AuthContext)
+	const [username, setUsername] = useState('')
+	const [password, setPassword] = useState('')
+	function handleUsernameChange(e){
+		setUsername(e.target.value)
 	}
-
-	return(
-		<UserProvider value={name}>
-		<div>
-			<section>
-				<div label="fdsfsdbs">
+	function handlePasswordChange(e){
+		setPassword(e.target.value)
+	}
+	function handleLogin(e){
+		e.preventDefault()
+		auth.login(username)
+		auth.register()
+	}
+	function test(e){
+		e.preventDefault()
+		console.log(user)
+	}
+	return (
+		<div className="login">
+			<form>
+				<label>
+					Username
 					<input 
-						value={name}
-						onChange={handleNameChange}
+						type="text"
+						value={username}
+						onChange={handleUsernameChange}
+						/>
+				</label>
+				<br />
+				<label>
+					Password
+					<input 
+						type="password"
+						value={password}
+						onChange={handlePasswordChange}
 					/>
-				</div>
-			</section>
+				</label>
+				<br />
+				<input type="submit" value="login" onClick={handleLogin}/>
+				<input type="submit" value="USER" onClick={test}/>
+			</form>
 		</div>
-		<Home />
-		</UserProvider>
 	)
-}
+}	
