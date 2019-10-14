03d5ab20a9a691cfc2341986da0a01b7acf8589a
andy <thamandyy@gmail.com>
2019-10-13 21:13:35 -0400

    Simplified form inputs

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -6,23 +6,15 @@ import { UserContext } from '../context/user';
 export default function Login(){
 	const user = useContext(UserContext)
 	const auth = useContext(AuthContext)
-	const [username, setUsername] = useState('')
-	const [password, setPassword] = useState('')
-	function handleUsernameChange(e){
-		setUsername(e.target.value)
-	}
-	function handlePasswordChange(e){
-		setPassword(e.target.value)
-	}
+	const username = useFormInput('');
+	const password = useFormInput('');
+
 	function handleLogin(e){
 		e.preventDefault()
-		auth.login(username)
+		auth.login(username.value)
 		auth.register()
 	}
-	function test(e){
-		e.preventDefault()
-		console.log(user)
-	}
+
 	return (
 		<div className="login">
 			<form>
@@ -30,8 +22,7 @@ export default function Login(){
 					Username
 					<input 
 						type="text"
-						value={username}
-						onChange={handleUsernameChange}
+						{...username}
 						/>
 				</label>
 				<br />
@@ -39,14 +30,26 @@ export default function Login(){
 					Password
 					<input 
 						type="password"
-						value={password}
-						onChange={handlePasswordChange}
+						{...password}
 					/>
 				</label>
 				<br />
-				<input type="submit" value="login" onClick={handleLogin}/>
-				<input type="submit" value="USER" onClick={test}/>
+				<input type="submit" value="Login" onClick={handleLogin}/>
+				<input type="button" onclick="location.href='/register';" value="Register" />
 			</form>
 		</div>
 	)
-}	
+}
+
+function useFormInput(initialValue){
+	const [value, setValue] = useState(initialValue)
+	
+	function handleChange(e){
+		setValue(e.target.value)
+	}
+
+	return {
+		value,
+		onChange: handleChange
+	};
+}
