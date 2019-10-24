12bc359f47c5f7c6e5dbefb8b89a1c294b9163a1
andy <thamandyy@gmail.com>
2019-10-23 21:27:27 -0400

    Express installed and setup

---
+++ server/app.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+const logger = require('morgan');
+const PORT = process.env.PORT || 8080;
+app.use(logger("dev"));
+const bodyParser = require('body-parser');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+	extended: false
+}))
+
+app.use(express.static('build'));
+
+
+app.get('/login', (req, res) => {
+	res.sendFile(path.join(__dirname + '../index.html'))
+})
+app.get('/register', (req, res) => {
+	res.sendFile(path.join(__dirname + '../index.html'))
+})
+app.get('/', (req, res) => {
+	res.sendFile(path.join(__dirname + '../index.html'))
+})
+
+app.listen(PORT, () => console.log(`Listening on port ${PORT}`))
