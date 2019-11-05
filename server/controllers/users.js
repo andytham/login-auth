c7253aa440f47b5373dfcead391cb99c68f76e2b
andy <thamandyy@gmail.com>
2019-11-04 21:22:15 -0500

    Code split controllers

---
+++ server/controllers/users.js
@@ -0,0 +1,55 @@
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
+const Users = require('../models/Users')
+const usersController = {
+	index: () => {
+
+	},
+	create: (req, res) => {
+		let password = req.body.password,
+			email = req.body.email,
+			username = req.body.username
+		bcrypt.genSalt(saltRounds, function(err, salt) {
+			bcrypt.hash(password, salt, function(err, hash){
+				Users.create({
+					username: username,
+					password: hash,
+					email: email
+				}).then(user => {
+					res.json({
+						msg: "User creation success.",
+						success: true
+					})
+				}).catch(err => {
+					res.json({
+						msg: "Error.",
+						success: false
+					})
+				})
+			})
+		})	
+	},
+	login: (req, res) => {
+		Users.find({
+			username: req.body.username
+		}).then(user => {
+			bcrypt.compare(req.body.password, user.password, function(err, bcryptRes){
+				if (bcryptRes){
+					console.log("Login successful.")
+					res.json({
+						msg: "Login successful.",
+						success: true
+					})
+				} else {
+					console.log("Wrong username/password.")
+					res.status(401).send("Wrong username/password.")
+				}
+			})
+		}).catch(err => {
+			console.log("User not found.");
+			res.status(401).send("User not found.")
+		})
+	}	
+}
+
+module.exports = usersController;
