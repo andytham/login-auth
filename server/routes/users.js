9fd702ad5a10713b83caff98070124f594db8172
andy <thamandyy@gmail.com>
2019-11-05 23:25:27 -0500

    Changed controller naming convention'

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const usersController = require('../controllers/users')
+const usersController = require('../controllers/usersController')
 const usersRouter = express.Router();
 
 usersRouter.post('/login', usersController.login)
