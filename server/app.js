6b48881e7ef1f485a9edf9052003928989a5af61
andy <thamandyy@gmail.com>
2019-11-08 18:19:26 -0500

    Express session installed

--- server/app.js
+++ server/app.js
@@ -1,18 +1,35 @@
 const express = require('express');
 const app = express();
 const path = require('path');
-
-const logger = require('morgan');
 const PORT = process.env.PORT || 8080;
+
+// debugging
+const logger = require('morgan');
 app.use(logger("dev"));
+
 const bodyParser = require('body-parser');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
 	extended: false
 }))
 
-app.use(express.static('build'));
+// session
+const session = require('express-session');
 
+//TODO persistence using a session store
+
+app.use(express.static('build'));
+app.use(session({
+	secret: "secretSession",
+	resave: true,
+	saveUninitialized: true,
+	cookie: {
+		originalMaxAge: null,
+  	maxAge: 60 * 60 * 1000, //milliseconds
+    httpOnly: true,
+		// secure: true, // HTTPS
+	}
+}))
 
 app.get('/login', (req, res) => {
 	res.sendFile(path.join(__dirname + '../index.html'))
