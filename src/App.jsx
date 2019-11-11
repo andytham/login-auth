fa2ceb9d1ff129ba1aa8791914be9dfa3cd328f6
andy <thamandyy@gmail.com>
2019-11-10 21:12:19 -0500

    Restore on page load (stay logged in)

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,21 @@
-import React, { Suspense, useContext } from 'react'
+import React, { Suspense, useContext, useEffect } from 'react'
 import { UserContext } from './context/user'
+import { AuthContext } from './context/auth';
+
 const AuthenticatedApp = React.lazy(() => import('./Authenticated'))
 const UnauthenticatedApp = React.lazy(() => import('./Unauthenticated'))
+
 
 import './css/App.scss'
 
 export default function App() {
 	const user = useContext(UserContext)
+	const auth = useContext(AuthContext)
+
+	useEffect(() => {
+		auth.restore()
+	},[])
+
   return(
 		<Suspense fallback={<div>Loading...</div>}>
 			{user ? <AuthenticatedApp /> : <UnauthenticatedApp />}
